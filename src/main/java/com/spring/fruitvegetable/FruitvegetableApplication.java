f38849827e8e0fd1fc9cebf30ee034a5eb31effe
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-24 12:12:52 +0300

    Project set up

---
+++ src/main/java/com/spring/fruitvegetable/FruitvegetableApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fruitvegetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitvegetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitvegetableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/fruitvegetable/FruitvegetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fruitvegetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitvegetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
